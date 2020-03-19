b3217a254039da062bd414cec72ce8703bb32d18
raulgp97 <57761438+raulgp97@users.noreply.github.com>
2020-03-20 00:32:19 +0100

    Add files via upload

---
+++ ControlErrores/ControlErrores/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+/************************************
+ * Autor: Raúl González Parra
+ * Curso: 1DAM-T
+ * Asignación: TryCatch Finally con ficheros
+ * Fecha Asignación: 4 de Marzo de 2020
+ * **********************************/
+
+namespace ControlErrores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string line;
+            System.IO.StreamReader file = null;
+
+            try
+            {
+                //Lee el fichero y lo imprime linea por linea.
+                file = new System.IO.StreamReader(@"./datos.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+
+                    Console.WriteLine(line);
+                    counter++;
+                }
+            }
+            //Imprimen mensajes de error, según la excepción que se capture.
+            catch(FileNotFoundException f)
+            {
+                
+                Console.WriteLine(f);
+
+            }
+            catch (Exception e)
+            {
+               
+                Console.WriteLine(e);
+            }
+            //En caso de error o no, cierra el fichero para que no se quede abierto.
+            finally
+            {
+               
+               file.Close();
+                
+            }
+            //Imprime las lineas que se han leído.
+            System.Console.WriteLine("There were {0} lines.", counter);
+            //Suspende la pantalla.
+            System.Console.ReadLine();
+        }
+    }
+}
